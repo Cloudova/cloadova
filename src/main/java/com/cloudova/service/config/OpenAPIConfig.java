0879d019cd387bd99d2c7b5108f31ad3f5da1781
Soroosh Tanzadeh <soroosh@ternobo.com>
2022-10-27 15:46:26 +0330

    open api spec title update

--- src/main/java/com/cloudova/service/config/OpenAPIConfig.java
+++ src/main/java/com/cloudova/service/config/OpenAPIConfig.java
@@ -2,6 +2,7 @@ package com.cloudova.service.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +12,12 @@ public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+        OpenAPI openapi = new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer").bearerFormat("JWT")));
+        openapi.setInfo(new Info().title("Cloudova Application Service").version("0.1.0-Beta"));
+        return openapi;
     }
 }
